f5df72833846458efba76b3b1f10373029479d5a
tatyamyam <105143277+tatyamyam@users.noreply.github.com>
2023-07-31 19:20:53 +0900

    kadai_004を追加

---
+++ kadai_004/java_study/src/kadai_004/Score_Chapter04.java
@@ -0,0 +1,32 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// Aさんの点数
+		System.out.println("Aさんの点数は10点");
+		// Bさんの点数
+		System.out.println("Bさんの点数は20点");
+		// Cさんの点数
+		System.out.println("Cさんの点数は30点");
+		// Dさんの点数
+		System.out.println("Dさんの点数は40点");
+		// Eさんの点数
+		System.out.println("Eさんの点数は50点");
+		// Fさんの点数
+		System.out.println("Fさんの点数は60点");
+		// Gさんの点数
+		System.out.println("Gさんの点数は70点");
+		// Hさんの点数
+		System.out.println("Hさんの点数は80点");
+		// Iさんの点数
+		System.out.println("Iさんの点数は90点");
+		// Jさんの点数
+		System.out.println("Jさんの点数は100点");
+		
+		// テストの平均点
+		System.out.println("テストの平均点は" + ((10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100)/10) + "点");
+				
+	}
+
+}
