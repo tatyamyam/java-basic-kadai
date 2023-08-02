ee18cbf241b9e409084eddac3c19e3c13f1fe710
tatyamyam <105143277+tatyamyam@users.noreply.github.com>
2023-08-02 21:46:13 +0900

    kadai_008を修正
    30代以外の判定結果もコンソールに表示するように修正しました

--- kadai_008/java_study/src/kadai_008/Price_Chapter08.java
+++ kadai_008/java_study/src/kadai_008/Price_Chapter08.java
@@ -17,16 +17,15 @@ public class Price_Chapter08 {
 		int serviceCost_6 = 500; //上記以外
 		
 		switch(userAge) {
-		case 10,11,12,13,14,15,16,17,18,19 -> System.out.println("");
-		case 20,21,22,23,24,25,26,27,28,29 -> System.out.println("");
-		//今回はコンソールに出力するのは30代のみとする、30代以上はblank表示とする
+		case 10,11,12,13,14,15,16,17,18,19 -> System.out.println("10代の料金は" + serviceCost_1 + "円");
+		case 20,21,22,23,24,25,26,27,28,29 -> System.out.println("20代の料金は" + serviceCost_2 + "円");
 		case 30,31,32,33,34,35,36,37,38,39 -> System.out.println("30代の料金は" + serviceCost_3 + "円");
-		case 40,41,42,43,44,45,46,47,48,49 -> System.out.println("");
-		case 50,51,52,53,54,55,56,57,58,59 -> System.out.println("");
-		case 60,61,62,63,64,65,66,67,68,69 -> System.out.println("");
-		case 70,71,72,73,74,75,76,77,78,79 -> System.out.println("");
-		case 80,81,82,83,84,85,86,87,88,89 -> System.out.println("");
-		default -> System.out.println("");
+		case 40,41,42,43,44,45,46,47,48,49 -> System.out.println("40代の料金は" + serviceCost_3 + "円");
+		case 50,51,52,53,54,55,56,57,58,59 -> System.out.println("50代の料金は" + serviceCost_4 + "円");
+		case 60,61,62,63,64,65,66,67,68,69 -> System.out.println("60代の料金は" + serviceCost_4 + "円");
+		case 70,71,72,73,74,75,76,77,78,79 -> System.out.println("70代の料金は" + serviceCost_4 + "円");
+		case 80,81,82,83,84,85,86,87,88,89 -> System.out.println("80代の料金は" + serviceCost_5 + "円");
+		default -> System.out.println("こちらの年代の料金は" + serviceCost_6 + "円");
 		}
 
 	}
