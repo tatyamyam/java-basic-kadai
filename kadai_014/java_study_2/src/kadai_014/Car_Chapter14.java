a4cfdedab44894519e7e9f0e881161ac36ea1722
tatyamyam <105143277+tatyamyam@users.noreply.github.com>
2023-08-13 15:29:36 +0900

    kadai_014 Update

--- kadai_014/java_study_2/src/kadai_014/Car_Chapter14.java
+++ kadai_014/java_study_2/src/kadai_014/Car_Chapter14.java
@@ -8,15 +8,31 @@ public class Car_Chapter14 {
 	
 	//①車クラスは次のメソッドを持つ
 	public void gearChange(int afterGear) {
-		
 		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
-		
 	}
 	
 	//②車クラスは次のメソッドを持つ
 	public void run() {
-		speed = 30;
-		System.out.println("速度は時速" + this.speed + "kmです");
+		gear = 3;
+		switch(gear) {
+		case 1:
+			System.out.println("速度は時速は10kmです");
+			break;
+		case 2:
+			System.out.println("速度は時速は20kmです");
+			break;
+		case 3:
+			System.out.println("速度は時速は30kmです");
+			break;
+		case 4:
+			System.out.println("速度は時速は40kmです");
+			break;
+		case 5:
+			System.out.println("速度は時速は50kmです");
+			break;
+		default :
+			System.out.println("速度は時速10kmです");
+		}
 		
 	}
 }
